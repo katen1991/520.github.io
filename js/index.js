7863923f564b24d39f2a928ab1417e9537a6bc4f
katen1991 <xxzpxxzp123@gmail.com>
2017-05-19 22:52:48 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function() {
-
-	var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
+var target_date = new Date( "2017/5/19" ); 
+	//var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
 	var days, hours, minutes, seconds; // variables for time units
 
 	var countdown = document.getElementById("tiles"); // get tag element
